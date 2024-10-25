39f982e04a8dab3c6b4f695ff41da0481b3c6cf5
Sergio <spensob0410@g.educaand.es>
2024-10-25 19:27:40 +0200

    commit desde la nueva rama

--- index.php
+++ index.php
@@ -1,1 +1,2 @@
 <h1>Hello Git</h1>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, unde sequi. Ratione amet laudantium ad ullam provident saepe eum quas, officiis ducimus cum nam quia earum, deleniti alias quaerat dicta!
